224c17b203415a6b028d182abe4823328094d41a
conil26 <63237888+conil26@users.noreply.github.com>
2020-04-06 13:41:54 +0200

    Mod Clearcache
    French translation

---
+++ French/clearcache.php
@@ -0,0 +1,25 @@
+<?php
+
+// Language definitions used in Clear Cache Plugin
+$lang_clearcache = array(
+
+// General
+'Plugin title'			=>		'Clear Cache v1.1.0',
+'Plugin discription'	=>		'Ce plugin vous permet de régénérer aisément et simplement vos fichiers de cache FluxBB.',
+'1.4'					=>		'Cette fonction n\'est active que pour FluxBB 1.4.x, si vous utilisez FluxBB 1.5, ça provoquera une erreur.',
+
+// Regen
+'Cache regen'			=>		'Régénère tous les fichiers cache',
+'Ban regen'				=>		'Régénère le cache des bannissements',
+'Ranks regen'			=>		'Régénère le cache du rank',
+'Config regen'			=>		'Régénère le cache de config',
+'Quick regen'			=>		'Régénère le cache des raccourcis',
+
+// Cache cleared
+'Cache re-generated'	=>		'Cache régénéré',
+'Ban re-generated'		=>		'Cache bannissements régénéré',
+'Ranks re-generated'	=>		'Cache du rank régénéré',
+'Config re-generated'	=>		'Cache de config régénéré',
+'Quick re-generated'	=>		'Cache des raccourcis',
+'Go back'				=>		'Retour',
+);
